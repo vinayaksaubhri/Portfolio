9f88aa0f4b42539429bb4ae47dc3313a12ca4d1a
Vinayak <vinayaksaubhri@gmail.com>
2021-03-30 20:05:11 +0530

    adding button

--- portfolio/src/Components/Button.js
+++ portfolio/src/Components/Button.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import '../Css/app.css'
+import { FaSun } from "react-icons/fa";
+
+
+let app_button__sun_icon = {fontsize:"100%"};
 function Button() {
     return (
         <div className="app_button">
-            
+          <FaSun/>
         </div>
     )
 }
