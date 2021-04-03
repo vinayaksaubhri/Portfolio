2a8a68d2d4fe8750d1d3bc4986e9a3803301bc83
Vinayak <vinayaksaubhri@gmail.com>
2021-04-03 15:25:40 +0530

    changed folder struc.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Css/app.css';
+import Navbar from './Components/Navbar';
+import Home from './Contents/Home';
+import Button from './Components/Button';
+
+const App = () =>{
+    return (   
+    <>
+      <Navbar/>
+      <Home/>
+      <Button/>
+   </>);
+}
+ export default App;
