3a96913e90b71e56b0edc6f222bf6ca7f95487de
Vinayak <vinayaksaubhri@gmail.com>
2021-04-13 02:15:15 +0530

    about page html

--- src/Contents/About.js
+++ src/Contents/About.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import "../Css/app.css"
+import "../Css/about.css"
 
 function About() {
     return (
-        <div>
-             <h1>this is About page</h1>
-        </div>
+      <div className="Main About">
+          <text className="About__heading">About Me</text>
+          <div className="slider_outer"><div className="slider_inner"></div></div>
+          <name className="name">Vinayak <line className="line">Saubhri</line></name>
+          <p className="navbar__des About__des">FrontEnd Developer</p>
+          <p className="About__bio">I started my journey in the world of the computers form an young age. 
+            Now I’m 20 years old. Pursuing my Computer Science and Engineering Degree 
+            in Dronacharya College of Engineering. Web development is my center of interest.
+            I always love the idea of cross-plaform development.Being a Software Engineer is
+            more than just programming.It’s a chance to help other people through the power of
+            technology.Having this as a career gives me the power to influence other peoples life
+            through programs that could help them with day to day tasks.</p>
+          <div className="About__book_icon"><img src="../src/Images/book.png" alt="book icon"/></div>
+      </div>
     )
 }
 
--- src/Contents/Home.js
+++ src/Contents/Home.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import '../Css/home.css'
+import '../Css/app.css'
 import Typical from 'react-typical'
 import Social from '../Components/Social'
 
 function Home() {
     return (
-        <div className="Main">
-          <name className="Main__name">Vinayak <line className="Main__line">Saubhri</line></name>
+        <div className="Main Home">
+          <name className="name">Vinayak <line className="line">Saubhri</line></name>
           <Typical 
            steps={[ 'A Front-End Developer', 1000 , 'A UI/UX Designer',1000]}
            loop={Infinity}
            wrapper="div"
            className="Main_typingtext"/>
+           <div className="slider_outer"><div className="slider_inner"></div></div>
+
            <Social/>
         </div>
     )
