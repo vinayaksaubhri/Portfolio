21067e796cd23609e5a63027dfcee607152e178c
Vinayak <vinayaksaubhri@gmail.com>
2021-04-26 15:42:04 +0530

    about page

--- src/Contents/About.js
+++ src/Contents/About.js
@@ -6,22 +6,42 @@ import book_icon from "../Images/book.svg"
 function About() {
     return (
       <div className="Main About">
-          <text className="About__heading">About Me</text>
-          <div className="slider_outer"><div className="slider_inner"></div></div>
-          <name className="name">Vinayak <line className="line">Saubhri</line></name>
-          <p className="navbar__des About__des">FrontEnd Developer</p>
-          <p className="About__bio">I started my journey in the world of the computers form an young age. 
-            Now I’m 20 years old. Pursuing my Computer Science and Engineering Degree 
-            in Dronacharya College of Engineering. Web development is my center of interest.
+          <div className="top_container">
+            <text className="About__heading">About Me</text>
+            <div className="slider_outer About__slider_outer"><div className="slider_inner About__slider_inner"></div></div>
+            <name className="name About__name">Vinayak <line className="line">Saubhri</line></name>
+            <p className="navbar__des About__des">FrontEnd Developer</p>
+            <p className="About__bio">I started my journey in the world of the computers form an young age. 
+            Now I’m 20 years old. Pursuing my Computer Science Degree form Dronacharya College of Engineering. Web development is my center of interest.
             I always love the idea of cross-plaform development.Being a Software Engineer is
             more than just programming.It’s a chance to help other people through the power of
             technology.Having this as a career gives me the power to influence other peoples life
             through programs that could help them with day to day tasks.</p>
-          <h1>Education</h1>  
-          <div className="About__book_icon "><img src={book_icon} alt="book icon"/></div>
-          <div className="vr_line glass">
-                <div className="vr_inner inn1"></div>
-                <div className="vr_inner inn2"></div>
+          </div>
+            <div className="bottom_container"> 
+               <div className="education__container">
+                 <div className="About__book_icon"><img src={book_icon} alt="book icon"/></div>
+                 <div className="vr_line glass">
+                     <div className="vr_inner inn1"></div>
+                     <div className="vr_inner inn2"></div>
+                 </div>
+                 <h1 className="About__Education">Education</h1>    
+                 <h2 className="About__Btech">B.tech in CSE</h2>
+                 <p className="About__clgname">Dronacharya College of Engineering</p>
+                 <p>Aug 2019-2023</p>
+                 <h2>High School</h2>
+                 <p>Chiranjiv Bharti School</p>
+                 <p>May 2018</p>
+              </div>
+              <div className="skill__container">
+                <h2>Skills</h2>
+                <div>HTML</div>
+                <div>CSS</div>
+                <div>JavaScript</div>
+                <div>React</div>
+                <div>Node</div>
+                <div>TypeScript</div>
+              </div> 
             </div>
       </div>
     )
